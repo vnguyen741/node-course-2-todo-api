09e54980fc0aed3a97f03c65dcbaed235d9b4135
Vu Nguyen <vun@sfu.ca>
2018-09-16 17:35:56 -0700

    Playgound mongoose queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,36 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '6b9edea9b1084512195b9c5c11';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById(id).then((user) => {
+    if(!user) {
+        return console.log('User not found');
+    }
+    console.log('User By Id', user);
+}).catch((e) => console.log(e));
